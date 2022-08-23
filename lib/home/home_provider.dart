97f83e4f9d39a1934c84e75202b7ded0080e157b
hatanvir <tanvirhasan553@gmail.com>
2022-08-23 23:19:51 +0600

    code updated

--- lib/home/home_provider.dart
+++ lib/home/home_provider.dart
@@ -3,15 +3,9 @@ import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_flutter/post/post_screen.dart';
 import '../photo/photo_screen.dart';
 
-
-
 var navScreenList = [
   const PostScreen(),
   const PhotoScreen()
 ];
-
-final stringProvider = Provider((ref)=>'Hello Provider!');
-
-final counterProvider = StateProvider((ref) => 0);
 
 final selectedNavigationProvider = StateProvider((ref) => 0);
